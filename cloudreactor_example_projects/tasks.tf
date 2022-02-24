f00d11e6dca0e1c10f093d1236d8440b18c1863d
jtsay <jtsay@cloudreactor.io>
2022-02-23 23:15:00 -0800

    Add task role for Java Quickstart project

--- cloudreactor_example_projects/tasks.tf
+++ cloudreactor_example_projects/tasks.tf
@@ -3,3 +3,9 @@ module "python_task_role" {
   project_name = "cloudreactor-python-ecs-quickstart"
   environment = terraform.workspace
 }
+
+module "java_task_role" {
+  source = "../modules/ecs_task_role"
+  project_name = "cloudreactor-java-ecs-quickstart"
+  environment = terraform.workspace
+}
