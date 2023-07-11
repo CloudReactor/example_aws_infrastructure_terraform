8ab09887f169a9ec1c238d9e7fc356c94a71e58c
jtsay <jeff@cloudreactor.io>
2023-07-11 10:56:32 -0700

    Move provider block to versions.tf in public-ecr

--- public-ecr/ecr.tf
+++ public-ecr/ecr.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
 
 terraform {
   backend "s3" {
--- public-ecr/versions.tf
+++ public-ecr/versions.tf
@@ -2,7 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 5.7.0"
     }
   }
   required_version = ">= 1.2.5"
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
